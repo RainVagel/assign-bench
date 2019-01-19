a11682985dfd6fa13b753bac26553c78e088f878
Murfin <joonas.kriisk@gmail.com>
2019-01-19 20:29:28 +0200

    Baby steps for simulate()

--- simulator.py
+++ simulator.py
@@ -3,6 +3,8 @@ from car_generator import CarGenerator
 from passenger import create_passenger
 import time
 
+PASSENGER_INTERVAL = 10
+
 graph = grp.read_graph("graph.json")
 node_id_node = grp.node_id_to_node(graph)
 id_node = grp.node_to_node_id(graph)
@@ -36,6 +38,34 @@ print(list(reversed(test_car.path)))
 test_car.movement()
 # Testi, kui auto ja vend spawnivad samas kohas
 
-def simulate(graph, nr_of_cars, nr_of_passengers):
+def simulate(input_graph, nr_of_cars, nr_of_passengers):
+    # Replace hard-coded string with input graph
+    graph = grp.read_graph("graph.json")
+    node_id_node = grp.node_id_to_node(graph)
+    id_node = grp.node_to_node_id(graph)
+    
+    free_cars = []
+    driving_cars = []
+    waiting_passengers = []
+    picked_up_passengers = []
+    
     car_generator = CarGenerator(graph)
+    generated_cars = car_gen.generate_cars(nr_of_cars, node_id_node, graph)
+    
+    free_cars.extend(generated_cars)
+    
+    global PASSENGER_INTERVAL
+    diam = grp.get_diameter(graph)
+    counter_passenger = 0
+    passenger_id = 0
+    # while True is only for testing purpouses. Eventually will be handled by SimulatorManager
+    while True:
+        if counter_passenger % PASSENGER_INTERVAL == 0:
+            waiting_passengers.append(create_passenger(passenger_id, graph, diam))
+            passenger_id += 1
+        # After every 100 passengers created, increase the speed at which new passengers are created
+        if len(passenger_id) % 100 == 0:
+            PASSENGER_INTERVAL -= 1
+        counter_passenger += 1
+    
     #gene
